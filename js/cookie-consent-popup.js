42e209a2a74b7e02db1c5f0f9b5e1345617a20af
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2022-05-08 00:19:23 -0300

    Update cookie-consent-popup.js

--- js/cookie-consent-popup.js
+++ js/cookie-consent-popup.js
@@ -29,7 +29,7 @@ window.onload = () => {
     if (shouldShowPopup(storageType)) {
         setTimeout(() => {
             consentPopup.classList.remove('hidden');
-        }, 60*1000);
+        }, 1000);
     }
 
 };
